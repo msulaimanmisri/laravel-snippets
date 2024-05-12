f233cda31e71b8833786123a33bcf41b5c529f69
Muhamad Sulaiman Misri <47172118+msulaimanmisri@users.noreply.github.com>
2024-05-12 20:59:50 +0800

    Create modern-web-application.js

---
+++ modern-web-application.js
@@ -0,0 +1,7 @@
+1. Modern web application use SPA (Single Page Application)
+
+2. We no need to reload the whole page when request to the server. Instead only return a Json file
+
+3. Based on my research, both SPA and MPA (Multi Page Application) has pros and cons
+
+4. If you want to have a SEO-friendly web app, go for MPA. Otherwise, choose SPA
