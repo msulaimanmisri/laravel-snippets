f1701528f6ec2c97d48788493bd4efa6e6118cf5
Muhamad Sulaiman Misri <47172118+msulaimanmisri@users.noreply.github.com>
2024-05-30 14:32:37 +0800

    Update many-to-many.php

--- many-to-many.php
+++ many-to-many.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+// Read : https://laravel.com/docs/11.x/eloquent-relationships#many-to-many
+
 /**
  * There are 2 ways to make our Many-to-Many relationship easy to implement, easy to read
  * First, we use `constrained` method
