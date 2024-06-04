d3227fd2a561f7e4158d29779a9ebb8ce6959181
Muhamad Sulaiman Misri <47172118+msulaimanmisri@users.noreply.github.com>
2024-06-04 15:01:21 +0800

    Update prepare-validation.php

--- prepare-validation.php
+++ prepare-validation.php
@@ -1,19 +1,12 @@
 <?php
 
 class SomeRequest extends FormRequest {
-  
-    public function rules() {
-        return [
-            'title' => ['required', 'string'],
-            'body' => ['required', 'string']
-        ];
-    }
 
-    /**
+  /**
      * If we wanted to do some action for the rules and before it's arrived at our business logic,
      * we can use this `prepareForValidation` method.
      * 
-     * Let's see the use case. We need to make all the value given as lower-case. So that later
+     * Let's see the use case. We need to make all the values given lower-case. So that later
      * in DB, all data coming from the user will become lower-case
      */
     protected function prepareForValidation()
@@ -25,4 +18,14 @@ class SomeRequest extends FormRequest {
         ]);
     }
   
+
+  /**
+  * Then we continue with the rules
+  */
+    public function rules() {
+        return [
+            'title' => ['required', 'string'],
+            'body' => ['required', 'string']
+        ];
+    }
 }
