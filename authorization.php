158a1db7a818ebeaac2d620506a02fb9ce8a23d2
Muhamad Sulaiman Misri <47172118+msulaimanmisri@users.noreply.github.com>
2024-06-18 15:01:16 +0800

    Update authorization.php

--- authorization.php
+++ authorization.php
@@ -14,11 +14,11 @@ class SomeController {
          /**
          * Another way to check if the Posts is belongs to the Authenticated user
          */
-        $notTheUser = $someClass->firstData->secondData->isNot(Auth::user());
-        abort_if($notTheUser, message: "This is not your Post!");
+        $notTheUser = $someClass->firstData->secondData->isNot( Auth::user() );
+        abort_if( $notTheUser, message: "This is not your Post!" );
 
         /**
-         * You also may need to use Laravel Gates @ Policy to make your Code more robust
+         * You also may need to use Laravel Gates @ Policy to make your code more robust
          * 
          * @link https://laravel.com/docs/11.x/authorization#gates
          * @link https://laravel.com/docs/11.x/authorization#creating-policies
